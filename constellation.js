2a7da234ca8637477f68a913150ffe6e0d26f467
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-03-07 13:57:59 -0500

    Working autogen

--- constellation.js
+++ constellation.js
@@ -9,23 +9,39 @@ var ctx = canvas.getContext('2d');
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-function Star(x, y, r, colour) {
+function Star(x, y, r) {
   this.x = x;
+  // NOTE: use percentage value
   this.y = y;
   this.r = r;
-  this.colour = colour;
+  this.colour = chooseColour(); // random colour
+  this.alpha = 0; // start at zero
+}
+
+function chooseColour() {
+  let colours = ["#e9e9e9", "#b6d3e9", "#ec8b69"]
+  return (colours[Math.floor(Math.random()*(colours.length+1))])
 }
 
 Star.prototype.constructor = Star;
 
 let stars = []
 
+
 function printMousePos(event) {
     x = event.clientX;
     y = event.clientY;
 
-    stars.push(Star(x, y, 10, "#e9e9e9"))
-  }
+    newStar(x, y)
+}
+
+function randomStar() {
+  newStar(Math.floor(Math.random()*(window.innerWidth)), Math.floor(Math.random()*(window.innerHeight)));
+}
+
+function newStar(x, y) {
+  stars.push(new Star(x/window.innerWidth, y/window.innerHeight, 5))
+}
   
 document.addEventListener("click", printMousePos); // add event
 
@@ -36,19 +52,36 @@ function draw() {
 
     //ctx.clearRect(0, 0, canvas.width, canvas.height); // clear rectangle
     
+    /*
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = '5';
     ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+    Debug
+    */ 
 
-    stars.forEach(function(item, index, array) {
+    stars.forEach(function(item, index, arr) {
+
+
+
       ctx.beginPath();
-      ctx.arc(item.x, item.y, item.r, 0, 2 * Math.PI);
+      ctx.globalAlpha = item.alpha
+
+      ctx.arc(item.x*window.innerWidth, item.y*window.innerHeight, item.r, 0, 2 * Math.PI);
       ctx.fillStyle = item.colour;
       ctx.fill();
+
+      item.alpha += 0.01
+
+      ctx.globalAlpha = 1
       ctx.closePath();
     })
-
-    
-
+  
   }
+
+  setInterval(randomStar, 1500);
+  // star generator
+
   setInterval(draw, 10);
+  // drawer
+
+  
